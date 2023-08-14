b46d784aff2d52f6220ae948eced4008e8302ace
Shreyas601 <97294953+Shreyas601@users.noreply.github.com>
2023-08-14 23:06:49 +0530

    Delete Exp1 directory

--- Exp1/exp1_7.m
+++
@@ -1,7 +0,0 @@
-a = 1+mod(236,3);
-data1 = importdata('Track002.wav');
-[f1,Fs1] = audioread('Track002.wav');
-f2 = importdata('ConvFile2.txt');
-y = conv(f1, f2);
-y_normalized = y/(max(abs(y)));
-audiowrite("filter.wav",y_normalized,Fs1);%high pass filter(rain drops more dominant)
